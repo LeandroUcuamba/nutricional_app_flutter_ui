c6d53c23bdc85775a4f7fa895ba3346823d4d384
Leandro Ucuamba <104640070+LeandroUcuamba@users.noreply.github.com>
2023-04-18 08:19:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nutricional_app_flutter_ui
+ Desenvolvi este " Nutricional App " utilizando o Flutter. Layoult by Dribble.
---
+++ android/app/src/main/kotlin/com/example/nutricional_app_flutter_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nutricional_app_flutter_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
